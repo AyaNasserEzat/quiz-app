0c4fbad200ece265b4eeef8e6fdb0249cd3a1da5
aya nasser <ayanasser1853@gmail.com>
2023-12-17 14:00:57 -0800

    first commit

---
+++ lib/model.dart
@@ -0,0 +1,22 @@
+class Questions{
+  String text;
+  List<Answer>answerlist;
+  Questions(this.text,this.answerlist);
+
+}
+class Answer{
+  String text;
+  bool isCorrect;
+  Answer(this.text,this.isCorrect);
+
+}
+List<Questions>getQuestion(){
+  List<Questions>list=[];
+  list.add(Questions("who made flutter", [
+    Answer("google", true),
+        Answer("oppo", false),
+    Answer("samsumg", false),
+  ]), );
+  return list;
+  
+}
